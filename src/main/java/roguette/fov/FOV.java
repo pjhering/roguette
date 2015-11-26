13953e22c5f2103e5c3a39dcc3981e1a60b75016
Pete Hering <pjhering@gmail.com>
2015-11-26 14:03:27 -0500

    added FOV class

--- src/main/java/roguette/fov/FOV.java
+++ src/main/java/roguette/fov/FOV.java
@@ -1,4 +1,67 @@
 package roguette.fov;
 
-public class FOV {
+import java.awt.Point;
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class FOV {
+
+    private final int radius;
+    private final Set<Point> memory;
+    
+    public FOV(int radius) {
+        
+        this.radius = radius;
+        this.memory = new HashSet<>();
+    }
+    
+    public abstract boolean isOpaque(Point p);
+    
+    public Set<Point> getMemory() {
+        
+        return memory;
+    }
+    
+    public boolean isInLineOfSight(Point here, Point there) {
+        
+        Line line = new Line(here, there);
+        
+        for(Point p : line) {
+            
+            if(isOpaque(p)) {
+                
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    public Set<Point> getFieldOfVision(Point center) {
+        
+        Circle circle = new Circle(center, radius);
+        Set<Point> fov = new HashSet<>();
+        
+        for(Point perimeter : circle) {
+            
+            Line line = new Line(center, perimeter);
+            
+            LINE:
+            for(Point p : line) {
+                
+                if(isOpaque(p)) {
+                    
+                    if(isOpaque(p)) {
+                        
+                        memory.add(p);
+                        break LINE;
+                    }
+                    
+                    fov.add(p);
+                }
+            }
+        }
+        
+        return fov;
+    }
 }
