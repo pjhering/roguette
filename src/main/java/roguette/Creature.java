2ff05cb5cf7ffc65a2b4acc80cba5eaa7eeb6a1a
Pete Hering <pjhering@gmail.com>
2015-11-29 05:56:20 -0500

    added generic properties

--- src/main/java/roguette/Creature.java
+++ src/main/java/roguette/Creature.java
@@ -1,16 +1,32 @@
 package roguette;
 
+import java.util.HashMap;
+import java.util.Map;
 import static java.util.UUID.randomUUID;
 
 public class Creature {
 
     private final int id;
     private final int type;
+    private final Map<String, Object> properties;
 
     public Creature(int type) {
 
         this.id = randomUUID().hashCode();
         this.type = type;
+        this.properties = new HashMap<>();
+    }
+    
+    public <T> T get(String key, Class<T> c) {
+        return (T) properties.get(key);
+    }
+    
+    public <T> T set(String key, T value) {
+        return (T) properties.put(key, value);
+    }
+    
+    public Object remove(String key) {
+        return properties.remove(key);
     }
 
     public int getType() {
--- src/main/java/roguette/Item.java
+++ src/main/java/roguette/Item.java
@@ -1,12 +1,29 @@
 package roguette;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Item {
 
     private final int type;
+    private final Map<String, Object> properties;
 
     public Item(int type) {
 
         this.type = type;
+        this.properties = new HashMap<>();
+    }
+    
+    public <T> T get(String key, Class<T> c) {
+        return (T) properties.get(key);
+    }
+    
+    public <T> T set(String key, T value) {
+        return (T) properties.put(key, value);
+    }
+    
+    public Object remove(String key) {
+        return properties.remove(key);
     }
 
     public int getType() {
